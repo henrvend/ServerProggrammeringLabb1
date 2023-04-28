f6e763e04ba90aedfeca8a139f089ed3b9e4c5a4
Henrik <henrvend@student.kau.se>
2023-04-28 09:54:30 +0200

    Hittar namn och färg efter submit

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -7,16 +7,42 @@ const jsDOM = require('jsdom');
 const cookieParser = require('cookie-parser');
 const globalObject = require('./servermodules/game-modul.js');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Använd det här istället
+// Sätter rot-access från '/', så att man kommer åt filerna från webben
+// och man kan skriva in ex. localhost:3000/html/index.html och nå sidan, 
+// utan att man skickas dit.
+app.use('/', express.static(__dirname +('/static')));
 
-app.use(express.static('static'));
+//använder coockieParser som middleware
+app.use(cookieParser());
+
+//använder bodyparser som middleware, låter post functionen automatiskt omvandla 
+//json till strängar som kan hämtas 
+app.use(bodyParser());
+
 
 app.get('/', (req, res) => {
   console.log('hello there');
-  res.sendFile(__dirname + '/static/html/index.html');
+  res.sendFile(__dirname + '/static/html/loggaIn.html');
 });
+
+
+//skapar ett post-anrop 
+//här ska valideringen av uppgifter ske när de hämtas in från formuläret
+app.post('/', (req, res) => {
+    console.log('inloggad');
+    let name = req.body.nick_1;
+    let color = req.body.color_1;
+    console.log(name);
+    console.log(color);
+    res.sendFile(__dirname + '/static/html/index.html');
+});
+
+
 
 
 app.listen('3000', () => {
